682bfa91d612bf9a4b50f94eace93139f2b7a238
fubiknees <85858509+fubiknees@users.noreply.github.com>
2024-01-22 19:15:19 -0800

    Grid created.Layoutredone via javascript per instructions.

--- Etch.js
+++ Etch.js
@@ -1,39 +1,67 @@
-const containerDiv = document.querySelector('.container')
+//create Variabe that links to 'body' of html 
+const body = document.querySelector('body');
 
+//create header div for selectSize input and submit button
+const header = document.createElement('div');
+header.classList.add('header');
+body.appendChild(header);
 
+//Text input box instructions
+header.textContent = "Grid Size: ";
 
+//create a Text Input Box to 'selectSize'
+let input = document.createElement('input');
+input.classList.add('input');
+input.setAttribute('type','text');
+input.setAttribute('value','16');
+header.appendChild(input);
 
+//Create Submit button to finalize select size input
+const btnSubmit = document.createElement('button')
+btnSubmit.textContent = "Submit";
+header.appendChild(btnSubmit);
 
-// // changing grid function to something i can better understand
-// function makeGrid(e) {
-//   for (let i = 0; i < e; i++) {
-//     const row = document.createElement('Div');
-//     container.appendChild(row);
-//     row.className = "row";
-//     for (let i = 0; i < e; i++) {
-//         const column = document.createElement('Div');
-//         row.appendChild(column);
-//         column.className = "column";
-//         column.style.width = '100%';
-//         let height = 960/ parseInt(columnValue);
-// //         column.style.height = `${height}px`;
-//     }
-//   }  
-// }
+//create a middle div 'container' to accomodate grid
+const container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
 
+//create footer div for button options ( black,rainbow, reset)
+const buttons = document.createElement('div')
+buttons.classList.add('buttons');
+body.appendChild(buttons);
+//starter value for create grid --> 16x16
+const value = 16;
 
-function draw ( ) {
-  background(220);
+//create button options
+const btnBlack = document.createElement('button');
+btnBlack.textContent = "Black";
+buttons.appendChild(btnBlack);
 
-  for (let x = 0;x < 20; x++) {
-    rect(x * 20,0,20,20);
+const btnRb = document.createElement('button');
+btnRb.textContent = "Rainbow";
+buttons.appendChild(btnRb);
+
+const btnClear = document.createElement('button');
+btnClear.textContent = "Clear";
+buttons.appendChild(btnClear);
+
+//create grid formula
+function createGrid (value) {
+ for( let i = 0; i < value; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+    for (let j = 0; j < value; j++) {
+        let column = document.createElement('row');
+        column.classList.add('column')
+        row.appendChild(column);
+    }
   }
-
-  for (let y = 0;y < 20; y ++) {
-    rect(0, y *20, 20, 20);
-  } 
 }
 
-draw( );
+createGrid (value);
 
-;
+
+
+
